63bbe597f2d51f634c7edc9ae7577cefa75087bc
FallenGod <danil.sidorov16@gmail.com>
2022-01-05 19:06:42 +0300

    [0003] Created base of Lesson model

---
+++ Models/Lesson.cs
@@ -0,0 +1,20 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using System;
+
+namespace WorkManager.Models
+{
+    public class Lesson
+    {
+        public Student Student { get; private set; }
+        public DateTime Date { get; private set; }
+        public readonly Event BaseEvent;
+        public Lesson(Event based)
+        {
+            BaseEvent = based;
+            Date = based.Start.DateTime.Value;
+
+            //TODO: Сослаться на парсера XML
+            //Найти по данным студента
+        }
+    }
+}

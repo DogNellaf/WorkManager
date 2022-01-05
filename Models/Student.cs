9b5a739d1ac532ed4e5829e6da899be625101ba9
FallenGod <danil.sidorov16@gmail.com>
2022-01-05 18:49:38 +0300

    [0002] Added student model

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkManager.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Duration { get; set; }
+        public double PricePerMinute => Price / Duration;
+
+        public override string ToString()
+        {
+            return $"Ученик {Name}, ставка {Price}/{Duration} мин. ({PricePerMinute} за минуту)";
+        }
+    }
+}
